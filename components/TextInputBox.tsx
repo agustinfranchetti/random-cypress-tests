67c996d536414fb37b7c56db0dd2741a265997b8
agustinfranchetti <agustinfranchetti@gmail.com>
2023-02-24 13:19:51 -0300

    working i think

---
+++ components/TextInputBox.tsx
@@ -0,0 +1,76 @@
+import { Box, Flex, Input, Button, Stack, Center } from "@chakra-ui/react";
+import { useClipboard } from "@chakra-ui/react";
+import { useState } from "react";
+import { parseText } from "../utils/parseText";
+export const TextInputBox = () => {
+  const [inputText, setInputText] = useState("");
+  const [linesAmmount, setLinesAmmount] = useState(undefined);
+  const { onCopy, value, setValue, hasCopied } = useClipboard("");
+  const handleParse = () => {
+    const parsedText = parseText(inputText, linesAmmount);
+    setValue(parsedText);
+  };
+
+  return (
+    <Center h="100vh" w="100vw">
+      <Box
+        alignItems="center"
+        justifyContent="center"
+        width="700px"
+        height="600px"
+        bgColor={"gray.100"}
+        borderRadius="lg"
+        boxShadow="lg"
+        p={4}
+      >
+        <Flex
+          flexDirection="row"
+          alignItems="center"
+          justifyContent="center"
+          width="100%"
+          height="100%"
+          gridColumn={3}
+          gap={4}
+        >
+          <Stack h={"100%"} w={"100%"}>
+            <Input
+              placeholder="How many lines do you want?"
+              value={linesAmmount}
+              onChange={(e) => setLinesAmmount(parseInt(e.target.value))}
+              width="100%"
+              height="10%"
+              gridColumn={3}
+              bgColor={"white"}
+              type="number"
+            />
+            <Input
+              placeholder="Paste your text here"
+              value={inputText}
+              onChange={(e) => setInputText(e.target.value)}
+              width="100%"
+              height="90%"
+              bgColor={"white"}
+            />
+          </Stack>
+          <Button onClick={handleParse} variant="solid" colorScheme="teal">
+            Parse
+          </Button>
+          <Stack h={"100%"} w={"100%"}>
+            {/* <Button onClick={onCopy} variant="solid" colorScheme="teal">
+              {hasCopied ? "Copied" : "Copy"}
+            </Button> */}
+            <Input
+              placeholder="Your parsed text will appear here"
+              value={value}
+              width="100%"
+              height="100%"
+              bgColor={"white"}
+              wordBreak="break-all"
+              overflowWrap={"break-word"}
+            />
+          </Stack>
+        </Flex>
+      </Box>
+    </Center>
+  );
+};
---
+++ utils/parseText.js
@@ -0,0 +1,23 @@
+export const parseText = (inputText, outputLinesAmount) => {
+  // Remove all whitespace from the input string
+  inputText = inputText.replace(/\s/g, "");
+  // Split the input string into an array of lines
+  const allLines = inputText
+    .split(/(?=cypress)/g)
+    .filter((line) => line !== "");
+
+  // Shuffle the array using the Fisher-Yates algorithm
+  for (let i = allLines.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [allLines[i], allLines[j]] = [allLines[j], allLines[i]];
+  }
+
+  // Pick the first numLinesToPick lines from the shuffled array
+  const pickedLines = allLines.slice(0, outputLinesAmount);
+
+  // Concatenate the picked lines into a single string separated by commas
+  const pickedLinesString = pickedLines.join(",");
+  const outputString = `npx cypress-repeat run -n 1 --spec ${pickedLinesString}`;
+  // Output the final string
+  return outputString;
+};
